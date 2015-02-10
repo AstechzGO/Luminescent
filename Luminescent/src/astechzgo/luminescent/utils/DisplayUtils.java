dda7ff6c9b6dbd181665e86f9cad6c6086bb23a2
eandr127 <rebl10@hotmail.com>
2015-02-09 20:00:16 -0500

    Fix formatting

--- Luminescent/src/astechzgo/luminescent/utils/DisplayUtils.java
+++ Luminescent/src/astechzgo/luminescent/utils/DisplayUtils.java
@@ -14,137 +14,159 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 public class DisplayUtils {
-	
+
 	public static final DisplayMode START_DISPLAY = Display.getDisplayMode();
-	
+
 	/**
-	 * Set the display mode to be used 
+	 * Set the display mode to be used
 	 * 
-	 * @param width The width of the display required
-	 * @param height The height of the display required
-	 * @param fullscreen True if we want fullscreen mode
+	 * @param width
+	 *            The width of the display required
+	 * @param height
+	 *            The height of the display required
+	 * @param fullscreen
+	 *            True if we want fullscreen mode
 	 */
 	public static void setDisplayMode(int width, int height, boolean fullscreen) {
-	 
-	    // return if requested DisplayMode is already set
-	    if ((Display.getDisplayMode().getWidth() == width) && 
-	        (Display.getDisplayMode().getHeight() == height) && 
-	    (Display.isFullscreen() == fullscreen)) {
-	        return;
-	    }
-	 
-	    try {
-	        DisplayMode targetDisplayMode = null;
-	         
-	    if (fullscreen) {
-	        DisplayMode[] modes = Display.getAvailableDisplayModes();
-	        int freq = 0;
-	                 
-	        for (int i=0;i<modes.length;i++) {
-	            DisplayMode current = modes[i];
-	                     
-	        if ((current.getWidth() == width) && (current.getHeight() == height)) {
-	            if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
-	                if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
-	                targetDisplayMode = current;
-	                freq = targetDisplayMode.getFrequency();
-	                        }
-	                    }
-	 
-	            // if we've found a match for bpp and frequence against the 
-	            // original display mode then it's probably best to go for this one
-	            // since it's most likely compatible with the monitor
-	            if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
-	                        (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
-	                            targetDisplayMode = current;
-	                            break;
-	                    }
-	                }
-	            }
-	        } else {
-	            targetDisplayMode = new DisplayMode(width,height);
-	        }
-	 
-	        if (targetDisplayMode == null) {
-	            System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
-	            return;
-	        }
-	 
-	        Display.setDisplayMode(targetDisplayMode);
-	        Display.setFullscreen(fullscreen);
-	             
-	    } catch (LWJGLException e) {
-	        System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
-	    }
+
+		// return if requested DisplayMode is already set
+		if ((Display.getDisplayMode().getWidth() == width)
+				&& (Display.getDisplayMode().getHeight() == height)
+				&& (Display.isFullscreen() == fullscreen)) {
+			return;
+		}
+
+		try {
+			DisplayMode targetDisplayMode = null;
+
+			if (fullscreen) {
+				DisplayMode[] modes = Display.getAvailableDisplayModes();
+				int freq = 0;
+
+				for (int i = 0; i < modes.length; i++) {
+					DisplayMode current = modes[i];
+
+					if ((current.getWidth() == width)
+							&& (current.getHeight() == height)) {
+						if ((targetDisplayMode == null)
+								|| (current.getFrequency() >= freq)) {
+							if ((targetDisplayMode == null)
+									|| (current.getBitsPerPixel() > targetDisplayMode
+											.getBitsPerPixel())) {
+								targetDisplayMode = current;
+								freq = targetDisplayMode.getFrequency();
+							}
+						}
+
+						// if we've found a match for bpp and frequence against
+						// the
+						// original display mode then it's probably best to go
+						// for this one
+						// since it's most likely compatible with the monitor
+						if ((current.getBitsPerPixel() == Display
+								.getDesktopDisplayMode().getBitsPerPixel())
+								&& (current.getFrequency() == Display
+										.getDesktopDisplayMode().getFrequency())) {
+							targetDisplayMode = current;
+							break;
+						}
+					}
+				}
+			} else {
+				targetDisplayMode = new DisplayMode(width, height);
+			}
+
+			if (targetDisplayMode == null) {
+				System.out.println("Failed to find value mode: " + width + "x"
+						+ height + " fs=" + fullscreen);
+				return;
+			}
+
+			Display.setDisplayMode(targetDisplayMode);
+			Display.setFullscreen(fullscreen);
+
+		} catch (LWJGLException e) {
+			System.out.println("Unable to setup mode " + width + "x" + height
+					+ " fullscreen=" + fullscreen + e);
+		}
 	}
-	
+
 	/**
 	 * Sets the icons for display
-	 * @param nIcon The locations of the icons
-	 * @param c A random object
+	 * 
+	 * @param nIcon
+	 *            The locations of the icons
+	 * @param c
+	 *            A random object
 	 */
 	public static void setIcons(String[] nIcon, Object c) {
-	    List<Image> icons = new ArrayList<Image>();    
-	    for(String path : nIcon) {
-	        Image img = new ImageIcon(c.getClass().getResource( "/resources/icons/" + path + ".png")).getImage();
-	        icons.add(img);
-	    }
-	    
-	    Display.setIcon(iconsToByteBuffers(icons));
+		List<Image> icons = new ArrayList<Image>();
+		for (String path : nIcon) {
+			Image img = new ImageIcon(c.getClass().getResource(
+					"/resources/icons/" + path + ".png")).getImage();
+			icons.add(img);
+		}
+
+		Display.setIcon(iconsToByteBuffers(icons));
 	}
-	
+
 	private static ByteBuffer[] iconsToByteBuffers(List<Image> icons) {
 		ByteBuffer[] b = new ByteBuffer[icons.size()];
-		
+
 		int i = 0;
-		for(Image icon : icons) {
-			b[i] = readImage(toBufferedImage(icon)) ;
+		for (Image icon : icons) {
+			b[i] = readImage(toBufferedImage(icon));
 			i++;
 		}
-		
+
 		return b;
 	}
-	
+
 	/**
 	 * Converts a given Image into a BufferedImage
 	 *
-	 * @param img The Image to be converted
+	 * @param img
+	 *            The Image to be converted
 	 * @return The converted BufferedImage
 	 */
 	private static BufferedImage toBufferedImage(Image img) {
-	    if (img instanceof BufferedImage) {
-	        return (BufferedImage) img;
-	    }
+		if (img instanceof BufferedImage) {
+			return (BufferedImage) img;
+		}
 
-	    // Create a buffered image with transparency
-	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		// Create a buffered image with transparency
+		BufferedImage bimage = new BufferedImage(img.getWidth(null),
+				img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
 
-	    // Draw the image on to the buffered image
-	    Graphics2D bGr = bimage.createGraphics();
-	    bGr.drawImage(img, 0, 0, null);
-	    bGr.dispose();
+		// Draw the image on to the buffered image
+		Graphics2D bGr = bimage.createGraphics();
+		bGr.drawImage(img, 0, 0, null);
+		bGr.dispose();
 
-	    // Return the buffered image
-	    return bimage;
+		// Return the buffered image
+		return bimage;
 	}
-	
+
 	/**
 	 * Convert BufferedImage to ByteBuffer
-	 * @param image The BufferedImage to convert
+	 * 
+	 * @param image
+	 *            The BufferedImage to convert
 	 * @return The converted image
 	 */
 	private static ByteBuffer readImage(BufferedImage image) {
-        int[] var3 = image.getRGB(0, 0, image.getWidth(), image.getHeight(), (int[])null, 0, image.getWidth());
-        ByteBuffer var4 = ByteBuffer.allocate(4 * var3.length);
-        int[] var5 = var3;
-        int var6 = var3.length;
+		int[] var3 = image.getRGB(0, 0, image.getWidth(), image.getHeight(),
+				(int[]) null, 0, image.getWidth());
+		ByteBuffer var4 = ByteBuffer.allocate(4 * var3.length);
+		int[] var5 = var3;
+		int var6 = var3.length;
 
-        for (int var7 = 0; var7 < var6; ++var7) {
-            int var8 = var5[var7];
-            var4.putInt(var8 << 8 | var8 >> 24 & 255);
-        }
+		for (int var7 = 0; var7 < var6; ++var7) {
+			int var8 = var5[var7];
+			var4.putInt(var8 << 8 | var8 >> 24 & 255);
+		}
 
-        var4.flip();
-        return var4;
-    }
+		var4.flip();
+		return var4;
+	}
 }
