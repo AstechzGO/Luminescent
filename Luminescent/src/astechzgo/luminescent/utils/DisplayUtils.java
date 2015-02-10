82c30bb8245e452d677681adb0e19e1a4ae21943
eandr127 <rebl10@hotmail.com>
2015-02-09 19:56:07 -0500

    Added icon for window

--- Luminescent/src/astechzgo/luminescent/utils/DisplayUtils.java
+++ Luminescent/src/astechzgo/luminescent/utils/DisplayUtils.java
@@ -1,5 +1,14 @@
 package astechzgo.luminescent.utils;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -68,4 +77,74 @@ public class DisplayUtils {
 	        System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
 	    }
 	}
+	
+	/**
+	 * Sets the icons for display
+	 * @param nIcon The locations of the icons
+	 * @param c A random object
+	 */
+	public static void setIcons(String[] nIcon, Object c) {
+	    List<Image> icons = new ArrayList<Image>();    
+	    for(String path : nIcon) {
+	        Image img = new ImageIcon(c.getClass().getResource( "/resources/icons/" + path + ".png")).getImage();
+	        icons.add(img);
+	    }
+	    
+	    Display.setIcon(iconsToByteBuffers(icons));
+	}
+	
+	private static ByteBuffer[] iconsToByteBuffers(List<Image> icons) {
+		ByteBuffer[] b = new ByteBuffer[icons.size()];
+		
+		int i = 0;
+		for(Image icon : icons) {
+			b[i] = readImage(toBufferedImage(icon)) ;
+			i++;
+		}
+		
+		return b;
+	}
+	
+	/**
+	 * Converts a given Image into a BufferedImage
+	 *
+	 * @param img The Image to be converted
+	 * @return The converted BufferedImage
+	 */
+	private static BufferedImage toBufferedImage(Image img) {
+	    if (img instanceof BufferedImage) {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
+	
+	/**
+	 * Convert BufferedImage to ByteBuffer
+	 * @param image The BufferedImage to convert
+	 * @return The converted image
+	 */
+	private static ByteBuffer readImage(BufferedImage image) {
+        int[] var3 = image.getRGB(0, 0, image.getWidth(), image.getHeight(), (int[])null, 0, image.getWidth());
+        ByteBuffer var4 = ByteBuffer.allocate(4 * var3.length);
+        int[] var5 = var3;
+        int var6 = var3.length;
+
+        for (int var7 = 0; var7 < var6; ++var7) {
+            int var8 = var5[var7];
+            var4.putInt(var8 << 8 | var8 >> 24 & 255);
+        }
+
+        var4.flip();
+        return var4;
+    }
 }
