01364677e6a044b0e774229f76317b957088e96b
Tylar Cunningham <T10Cunningham@gmail.com>
2015-02-15 17:58:22 -0500

    Added rendering utils, need to work on it more though. Also fixed taking screenshots.

---
+++ Luminescent/src/astechzgo/luminescent/utils/RenderingUtils.java
@@ -0,0 +1,26 @@
+package astechzgo.luminescent.utils;
+
+import org.lwjgl.opengl.GL11;
+
+public class RenderingUtils
+{
+	public static void RenderQuad(int x, int y, int width, int height)
+	{
+		GL11.glBegin(GL11.GL_QUADS);
+		
+		GL11.glColor3f(0.0f, 0.4f, 0.5f);
+		
+		GL11.glVertex2d(x, y);
+		GL11.glVertex2d(x + width, y);
+		GL11.glVertex2d(x + width, y + height);
+		GL11.glVertex2d(x, y + height);
+		
+		GL11.glEnd();
+	}
+	
+	public static void RenderTexturedQuad(int x, int y, int width, int height)
+	{
+//		GL11.glBindTexture(GL11.GL_TEXTURE_2D, somethingelse);
+		RenderQuad(x, y, width, height);
+	}
+}
