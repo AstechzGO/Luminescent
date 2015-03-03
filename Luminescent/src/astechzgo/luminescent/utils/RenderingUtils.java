3e9c960c307089964583952f73e488a49a6c3b11
eandr127 <rebl10@hotmail.com>
2015-03-02 20:50:31 -0500

    Fxed textures

--- Luminescent/src/astechzgo/luminescent/utils/RenderingUtils.java
+++ Luminescent/src/astechzgo/luminescent/utils/RenderingUtils.java
@@ -11,14 +11,15 @@ public class RenderingUtils
 	{
 		GL11.glBegin(GL11.GL_QUADS);
 		
-		GL11.glTexCoord2f(0,0);
+		GL11.glTexCoord2f(0,1);
 		GL11.glVertex2d(x, y);
-		GL11.glTexCoord2f(1,0);
-		GL11.glVertex2d(x + width, y);
 		GL11.glTexCoord2f(1,1);
+		GL11.glVertex2d(x + width, y);
+		GL11.glTexCoord2f(1,0);
 		GL11.glVertex2d(x + width, y + height);
-		GL11.glTexCoord2f(0, 1);
+		GL11.glTexCoord2f(0, 0);
 		GL11.glVertex2d(x, y + height);
+		
 		
 		GL11.glEnd();
 	}
@@ -31,8 +32,10 @@ public class RenderingUtils
 		if(texture.getAsSlickTexture() != null) 
 		{
 			Color.white.bind();
-			texture.getAsSlickTexture().bind();	
+			
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.getAsSlickTexture().getTextureID());
 			RenderQuad(x, y, texture.getAsSlickTexture().getImageWidth(), texture.getAsSlickTexture().getImageHeight());
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		}
 	}
 }
