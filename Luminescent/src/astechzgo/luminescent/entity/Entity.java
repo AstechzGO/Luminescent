1e8fb6b981c93f43d545cb06a1a2fb0317d53d50
Stealth2994 <cnnr252@gmail.com>
2016-01-25 12:56:08 -0500

    Basic entity framework
    personally I think we need to make entity an extension but ok

--- Luminescent/src/astechzgo/luminescent/entity/Entity.java
+++ Luminescent/src/astechzgo/luminescent/entity/Entity.java
@@ -1,5 +1,7 @@
 package astechzgo.luminescent.entity;
 
 public interface Entity {
-
+public void SetHealth(int health);
+public int GetHealth();
+public boolean IsDead();
 }
--- Luminescent/src/astechzgo/luminescent/entity/PolygonalEntity.java
+++ Luminescent/src/astechzgo/luminescent/entity/PolygonalEntity.java
@@ -11,5 +11,26 @@ public class PolygonalEntity extends RenderableMultipleRenderedObjects implement
 		super(objects);
 		// TODO Auto-generated constructor stub
 	}
+	public int health = 5;
+	public int damage = 2;
+	@Override
+	public void SetHealth(int health) {
+		// TODO Auto-generated method stub
+		this.health = health;
+	}
 
+	@Override
+	public int GetHealth() {
+		// TODO Auto-generated method stub
+		return health;
+	}
+
+	@Override
+	public boolean IsDead() {
+		// TODO Auto-generated method stub
+		if(health <= 0) {
+			return true;
+		} else
+			return false;
+	}
 }
