b04b62023b9043d2ed9d542fdc4a1fe1aed2df40
eandr127 <eandr127@outlook.com>
2016-03-27 20:20:42 -0400

    Gradle support

--- deploy.bat
+++ deploy.bat
@@ -1,32 +1,3 @@
 @echo off
-goto :start
-:start
-echo.
-echo Luminescent deployment tool by eandr127
-echo.
-cd Deployment/build
-
-if not exist ..\apache-ant-1.9.6 goto :error
-
-cd ..\
-build\fciv -v -xml build\out.bin -md5 apache-ant-1.9.6
-cd build
-
-if not %ERRORLEVEL%==0 goto :error
-
-echo.
-
-call ..\apache-ant-1.9.6\bin\ant.bat -f build.xml
-
-echo.
-
-echo The output jar has been placed in the Deployment folder
-pause
-
-goto :eof
-
-:error
-echo.
-echo ANT is either corrupt or does not exist.  Downloading now...
-call updateANT.bat
-pause
+cd Deployment
+gradlew deploy_jar
